fd05bf317233ca28266a6c5d33423c4f08c8d9b9
Ryogo.Yoshimura <Ryogo.Yoshimura@d8770963-c053-0410-a7e7-098f70d26b45>
2008-08-09 17:34:46 +0000

    CHG use boost::mutex NEW linear interpolation of hysteresis CHG gain -> pre gain NEW boost (x 10) parameter NEW post gain DBG remove bias from post gain CHG append hysteresis.cpp to project (because using boost::mutex lib, no need to "include" .cpp file)

--- hysteresis.cpp
+++ hysteresis.cpp
@@ -8,7 +8,6 @@
 // Description  : Hysteresis filter
 //
 // (c) Ryogo Yoshimura, All Rights Reserved
-// MIT License
 //-------------------------------------------------------------------------------------------------------
 
 #include <stdio.h>
@@ -25,9 +24,10 @@
 HysteresisProgram::HysteresisProgram ()
 {
 	// default Program Values
-	param.fGain=0.25;
-	param.iNdiv=1000;
+	param.fPreGain=0.25;
+	param.iBoost=0;
 	param.fParam1=0.5;
+	param.fPostGain=0.25;
 
 	strcpy (name, "Init");
 }
@@ -38,11 +38,11 @@ Hysteresis::Hysteresis (audioMasterCallback audioMaster)
 {
 	// init
 
-	internalhysteresis[0].resize(1000);
+	internalhysteresis[0].resize(100);
 	internalhysteresis[0].inithalf();
 	//internalhysteresis[0].genmu();
 
-	internalhysteresis[1].resize(1000);
+	internalhysteresis[1].resize(100);
 	internalhysteresis[1].inithalf();
 	//internalhysteresis[1].genmu();
 	
@@ -72,9 +72,10 @@ void Hysteresis::setProgram (VstInt32 program)
 	HysteresisProgram* ap = &programs[program];
 
 	curProgram = program;
-	setParameter (kGain, ap->param.fGain);
-	setParameter (kNdiv, ap->param.iNdiv);
+	setParameter (kPreGain, ap->param.fPreGain);
+	setParameter (kBoost, ap->param.iBoost);
 	setParameter (kParam1,ap->param.fParam1);
+	setParameter (kPostGain, ap->param.fPostGain);
 }
 
 //------------------------------------------------------------------------
@@ -117,9 +118,10 @@ void Hysteresis::setParameter (VstInt32 index, float value)
 
 	switch (index)
 	{
-		case kGain : setGain(value); break;
-		case kNdiv : setNdiv(value); break;
+		case kPreGain : setPreGain(value); break;
+		case kBoost : setBoost(value); break;
 		case kParam1:setParam1(value);break;
+		case kPostGain : setPostGain(value); break;
 	}
 }
 
@@ -130,9 +132,10 @@ float Hysteresis::getParameter (VstInt32 index)
 
 	switch (index)
 	{
-		case kGain : v = getGain(); break;
-		case kNdiv : v = getNdiv(); break;
+		case kPreGain : v = getPreGain(); break;
+		case kBoost : v = getBoost(); break;
 		case kParam1:v = getParam1();break;
+		case kPostGain : v = getPostGain(); break;
 	}
 	return v;
 }
@@ -142,9 +145,10 @@ void Hysteresis::getParameterName (VstInt32 index, char *label)
 {
 	switch (index)
 	{
-		case kGain : strcpy (label, "Gain");	break;
-		case kNdiv : strcpy (label, "Ndiv");	break;
+		case kPreGain : strcpy (label, "PreGain");	break;
+		case kBoost : strcpy (label, "x10");	break;
 		case kParam1:strcpy (label, "Param1");	break;
+		case kPostGain : strcpy (label, "PostGain");	break;
 	}
 }
 
@@ -153,9 +157,10 @@ void Hysteresis::getParameterDisplay (VstInt32 index, char *text)
 {
 	switch (index)
 	{
-		case kGain : dB2string (param.fGain*4, text, kVstMaxParamStrLen);	break;
-		case kNdiv : int2string (param.iNdiv, text, kVstMaxParamStrLen);	break;
+		case kPreGain : dB2string (param.fPreGain*4, text, kVstMaxParamStrLen);	break;
+		case kBoost : if(param.iBoost){strcpy(text,"On");}else{strcpy(text,"Off");}break;//int2string (param.iNdiv, text, kVstMaxParamStrLen);	break;
 		case kParam1:float2string (param.fParam1, text, kVstMaxParamStrLen);	break;
+		case kPostGain : dB2string (param.fPostGain*4, text, kVstMaxParamStrLen);	break;
 	}
 }
 
@@ -164,9 +169,10 @@ void Hysteresis::getParameterLabel (VstInt32 index, char *label)
 {
 	switch (index)
 	{
-		case kGain : strcpy (label, "dB");	break;
-		case kNdiv : strcpy (label, "");	break;
+		case kPreGain : strcpy (label, "dB");	break;
+		case kBoost : strcpy (label, "");	break;
 		case kParam1:strcpy (label, "");	break;
+		case kPostGain : strcpy (label, "dB");	break;
 	}
 }
 
@@ -194,55 +200,79 @@ bool Hysteresis::getVendorString (char* text)
 //---------------------------------------------------------------------------
 void Hysteresis::processReplacing (float** inputs, float** outputs, VstInt32 sampleFrames)
 {
-	Rydot::synchronized sync(cs);
+	//Rydot::synchronized sync(cs);
+	boost::mutex::scoped_lock sync(mtx);
+
+	float bst=(param.iBoost)?10:1;
 	for(int k=0;k<2;++k)
 	{
 		for(int i=0;i<sampleFrames;++i)
 		{
-			outputs[k][i]=internalhysteresis[k].op(inputs[k][i]*param.fGain*4*0.5+0.5)*2-1;
+			outputs[k][i]=
+				(
+					internalhysteresis[k].op
+						(inputs[k][i]*param.fPreGain*4*bst*0.5+0.5)
+					*2-1
+				)
+				*param.fPostGain*4;
 		}
 	}
 }
 
-void Hysteresis::setGain(float g)
+void Hysteresis::setPreGain(float g)
 {
 	HysteresisProgram* ap = &programs[curProgram];
-	ap->param.fGain=param.fGain=g;
+	boost::mutex::scoped_lock sync(mtx);
+	ap->param.fPreGain=param.fPreGain=g;
 }
 
-float Hysteresis::getGain()
+float Hysteresis::getPreGain()
 {
-	return param.fGain;
+	return param.fPreGain;
 }
 
-void Hysteresis::setNdiv(float n)
+void Hysteresis::setBoost(float n)
 {
 	HysteresisProgram* ap = &programs[curProgram];
-	n*=2000;
-	if(n>2000)n=2000;
-	if(n<3)n=3;
-	ap->param.iNdiv=param.iNdiv=n;
-	Rydot::synchronized sync(cs);
-	internalhysteresis[0].resize(param.iNdiv);
-	internalhysteresis[1].resize(param.iNdiv);
+	//n*=2000;
+	//if(n>2000)n=2000;
+	//if(n<3)n=3;
+	//ap->param.iNdiv=param.iNdiv=n;
+	ap->param.iBoost=param.iBoost=(n>0.5)?1:0;
+	//Rydot::synchronized sync(cs);
+	//internalhysteresis[0].resize(param.iNdiv);
+	//internalhysteresis[1].resize(param.iNdiv);
 }
 
-float Hysteresis::getNdiv()
+float Hysteresis::getBoost()
 {
-	return param.iNdiv/2000.0;
+	return param.iBoost;
 }
 
 void Hysteresis::setParam1(float p)
 {
 	HysteresisProgram* ap = &programs[curProgram];
 	ap->param.fParam1=param.fParam1=p;
-	Rydot::synchronized sync(cs);
+	//Rydot::synchronized sync(cs);
+	boost::mutex::scoped_lock sync(mtx);
 	internalhysteresis[0].setparam1(param.fParam1);
 	internalhysteresis[1].setparam1(param.fParam1);
 }
 float Hysteresis::getParam1()
 {
 	return param.fParam1;
+}
+
+void Hysteresis::setPostGain(float g)
+{
+	HysteresisProgram* ap = &programs[curProgram];
+	boost::mutex::scoped_lock sync(mtx);
+	ap->param.fPostGain=param.fPostGain=g;
+}
+
+float Hysteresis::getPostGain()
+{
+	return param.fPostGain;
 }
 
 
